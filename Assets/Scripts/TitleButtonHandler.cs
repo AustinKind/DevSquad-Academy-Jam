561a2c92fff2502f4babeafb0479f2078c5c57ba
Austin Kind <59539755+AustinKind@users.noreply.github.com>
2020-12-21 13:24:19 -0600

    Skeleton code

---
+++ Assets/Scripts/TitleButtonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonHandler : MonoBehaviour {
+    
+    public void newGame() {
+        SceneManager.LoadSceneAsync("Game Scene");
+    }
+
+    public void exitGame() {
+        Application.Quit();
+        Debug.Log("Game is exiting...");
+    }
+}
