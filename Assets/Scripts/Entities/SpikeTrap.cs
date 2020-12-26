a83ae9ced3cc8a73eabef5a3b249362810dfa2dd
Austin Kind <59539755+AustinKind@users.noreply.github.com>
2020-12-26 17:05:50 -0600

    Added Spike Trap

---
+++ Assets/Scripts/Entities/SpikeTrap.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    public Animator animator;
+
+    // How long the spikes are retracted for.
+    [SerializeField] private float idleTime = 4;
+    // How long the spikes are in the ready state.
+    [SerializeField] private float readyTime = 1;
+    // How long the spikes are up for.
+    [SerializeField] private float impaledTime = 2;
+    // How much damage the spikes do.
+    [SerializeField] private int damage = 1;
+
+    void Start ()
+    {
+        StartCoroutine("SpikeCycle");
+    }
+
+    IEnumerator SpikeCycle ()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(idleTime);
+            animator.SetTrigger("Ready");
+            yield return new WaitForSeconds(readyTime);
+            animator.SetTrigger("Impale");
+            yield return new WaitForSeconds(impaledTime);
+            animator.SetTrigger("Retract");
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Damageable dmgObj = null;
+            if ((dmgObj = col.GetComponent<Damageable>()) != null)
+            {
+                dmgObj.Hurt(damage);
+            }
+        }
+    }
+}
