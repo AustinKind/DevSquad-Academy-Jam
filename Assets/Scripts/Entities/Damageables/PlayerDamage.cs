e042d761c11392ba79723b733dbacafb2d658a25
Austin Kind <59539755+AustinKind@users.noreply.github.com>
2020-12-30 17:37:46 -0600

    Added invulnerability frames

--- Assets/Scripts/Entities/Damageables/PlayerDamage.cs
+++ Assets/Scripts/Entities/Damageables/PlayerDamage.cs
@@ -4,14 +4,54 @@ using UnityEngine;
 
 public class PlayerDamage : Damageable
 {
+    private SpriteRenderer renderer;
+    [SerializeField] private float invulnerabilityTime = 2f;
+    [SerializeField] private float intervalTime = 0.25f;
+    [SerializeField] private Color flashingColor = new Color(1f, 0.1f, 0.1f, 0.39f);
+    private bool isInvulnerable = false;
+    private bool looping = true;
+
     public override void Start()
     {
         base.Start();
+        renderer = transform.Find("CharacterSprite").gameObject.GetComponent<SpriteRenderer>();
     }
 
     public override void Hurt(int dmg)
     {
-        base.Hurt(dmg);
-        PlayerStatusManager.Instance.ModifyHealth(-dmg);
+        if (!isInvulnerable)
+        {
+            base.Hurt(dmg);
+            PlayerStatusManager.Instance.ModifyHealth(-dmg);
+            Knockback();
+            StartCoroutine("Invulnerable");
+        }
+    }
+
+    public void Knockback()
+    {
+        Debug.Log("Apply knockback!");
+    }
+
+    IEnumerator Invulnerable()
+    {
+        isInvulnerable = true;
+        StartCoroutine("Waiting");
+        while (looping)
+        {
+            renderer.color = flashingColor;
+            yield return new WaitForSeconds(intervalTime);
+            renderer.color = new Color(255, 255, 255, 1);
+            yield return new WaitForSeconds(intervalTime);
+        }
+        renderer.color = new Color(255, 255, 255, 1);
+        isInvulnerable = false;
+        looping = true;
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(invulnerabilityTime);
+        looping = false;
     }
 }
---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    private Text text;
+    private PlayerStatusManager psm;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        psm = PlayerStatusManager.Instance;
+    }
+
+    void Update()
+    {
+        text.text = psm.HitPoints + "";
+    }
+}
