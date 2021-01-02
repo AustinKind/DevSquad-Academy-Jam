484829165dca22700b24ec90018227ae9aa1aeb5
Austin Kind <59539755+AustinKind@users.noreply.github.com>
2021-01-02 00:14:17 -0600

    Created 2 levels, added spikes

---
+++ Assets/Scripts/Entities/Spikes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] public int damage = 1;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Damageable dmgObj = null;
+        if ((dmgObj = col.GetComponent<Damageable>()) != null)
+        {
+            PlayerDamage player = null;
+            if ((player = (dmgObj as PlayerDamage)) != null)
+                player.SetKnockback(new Vector2(player.transform.position.x - transform.position.x, 1f));
+            dmgObj.Hurt(damage);
+        }
+    }
+}
